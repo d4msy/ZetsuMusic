b5b565a3efc97a033fdfd294dc620f71340e5b51
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-16 15:49:53 +0700

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,8 +57,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("mahadappa")
-                await self.one.join_chat("pantekyks")
+                await self.one.join_chat("damprivateroom")
+                await self.one.join_chat("diarydam")
             except:
                 pass
             assistants.append(1)
@@ -87,8 +87,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("mahadappa")
-                await self.two.join_chat("pantekyks")
+                await self.two.join_chat("damprivateroom")
+                await self.two.join_chat("diarydam")
             except:
                 pass
             assistants.append(2)
@@ -117,8 +117,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("mahadappa")
-                await self.three.join_chat("pantekyks")
+                await self.three.join_chat("damprivateroom")
+                await self.three.join_chat("diarydam")
             except:
                 pass
             assistants.append(3)
@@ -147,8 +147,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("mahadappa")
-                await self.four.join_chat("pantekyks")
+                await self.four.join_chat("damprivateroom")
+                await self.four.join_chat("diarydam")
             except:
                 pass
             assistants.append(4)
@@ -177,8 +177,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("mahadappa")
-                await self.five.join_chat("pantekyks")
+                await self.five.join_chat("damprivateroom")
+                await self.five.join_chat("diarydam")
             except:
                 pass
             assistants.append(5)
