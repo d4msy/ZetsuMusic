98ca42baf331ff86dc746b9bc5b4b219e7570af1
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-19 14:53:03 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],  switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/damprivateroom",
+                text=_["S_B_9"], url="https://t.me/damprivateroom",
             ),
         ],
         [
@@ -34,6 +34,11 @@ def telegram_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(
                 text=_["PL_B_3"], switch_inline_query_current_chat=""
             ),
+            InlineKeyboardButton(
+                text=_["S_B_9"], url="https://t.me/damprivateroom",
+            ),
+        ],
+        [
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             ),
