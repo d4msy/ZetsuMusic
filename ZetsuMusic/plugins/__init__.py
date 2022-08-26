e45ac03d1e3868290809c1c9d4c5d5335b506b33
bdrl <bdrl>
2022-08-26 16:05:48 +0700

    asw lupa

--- ZetsuMusic/plugins/__init__.py
+++ ZetsuMusic/plugins/__init__.py
@@ -1,9 +1,9 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/damsyx/zetsumusicBot > project,
+# This file is part of < https://github.com/damsyx/zetsumusic > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/damsyx/zetsumusicBot/blob/master/LICENSE >
+# Please see < https://github.com/damsyx/zetsumusic/blob/master/LICENSE >
 #
 # All rights reserved.
 #
