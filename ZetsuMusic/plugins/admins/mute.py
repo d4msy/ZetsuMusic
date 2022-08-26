79302b9ab303a18bba925942da2d431de7d80a92
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-26 16:18:45 +0700

    Update mute.py

--- ZetsuMusic/plugins/admins/mute.py
+++ ZetsuMusic/plugins/admins/mute.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from ZetsuMusic import app
-from ZetsuMusic.core.call import Yukki
+from ZetsuMusic.core.call import Zetsu
 from ZetsuMusic.utils.database import is_muted, mute_on
 from ZetsuMusic.utils.decorators import AdminRightsCheck
 
@@ -34,7 +34,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"])
     await mute_on(chat_id)
-    await Yukki.mute_stream(chat_id)
+    await Zetsu.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention)
     )
