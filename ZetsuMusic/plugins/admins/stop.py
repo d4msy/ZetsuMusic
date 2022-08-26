17776d1bf8559524de96bad3ab9e0ec778fccf26
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-26 16:22:55 +0700

    Update stop.py

--- ZetsuMusic/plugins/admins/stop.py
+++ ZetsuMusic/plugins/admins/stop.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from ZetsuMusic import app
-from ZetsuMusic.core.call import Yukki
+from ZetsuMusic.core.call import Zetsu
 from ZetsuMusic.utils.database import set_loop
 from ZetsuMusic.utils.decorators import AdminRightsCheck
 
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await Zetsu.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
