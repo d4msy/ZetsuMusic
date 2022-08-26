b18a5cad3338e9d36fc3beadc0978cdcabfaab29
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-26 16:12:12 +0700

    Update bot.py

--- ZetsuMusic/core/bot.py
+++ ZetsuMusic/core/bot.py
@@ -16,7 +16,7 @@ import config
 from ..logging import LOGGER
 
 
-class YukkiBot(Client):
+class ZetsuBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
