f4bc19d4f40d0aeb145c9e891c32807181230c34
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-26 16:51:51 +0700

    Update pastebin.py

--- ZetsuMusic/utils/pastebin.py
+++ ZetsuMusic/utils/pastebin.py
@@ -22,7 +22,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def Yukkibin(text):
+async def Zetsubin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
