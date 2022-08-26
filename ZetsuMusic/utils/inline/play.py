cd05fb3aeaa7a3ddbc9ada0ac786cac60ab1c9ac
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-26 16:46:45 +0700

    Update play.py

--- ZetsuMusic/utils/inline/play.py
+++ ZetsuMusic/utils/inline/play.py
@@ -118,11 +118,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"YukkiPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"ZetsuPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"YukkiPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"ZetsuPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
