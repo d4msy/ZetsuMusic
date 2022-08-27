2939d813531dc3e4e2702638bcae95994cb71992
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-08-27 17:41:03 +0700

    Update ping.py

--- ZetsuMusic/plugins/tools/ping.py
+++ ZetsuMusic/plugins/tools/ping.py
@@ -15,7 +15,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
 from ZetsuMusic import app
-from ZetsuMusic.core.call import Yukki
+from ZetsuMusic.core.call import Zetsu
 from ZetsuMusic.utils import bot_sys_stats
 from ZetsuMusic.utils.decorators.language import language
 
@@ -36,7 +36,7 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"],
     )
     start = datetime.now()
-    pytgping = await Yukki.ping()
+    pytgping = await Zetsu.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
